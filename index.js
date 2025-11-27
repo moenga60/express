af5e54579d1a9292dfdab6045eeef5524e2cee50
moenga60 <moengadaniel60@gmail.com>
2025-11-27 11:52:16 +0300

    routing: dynamic & query

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require("express");
+import express from "express";
 
 const app = express();
 
@@ -6,7 +6,12 @@ const PORT = 3000;
 
 // simple route
 app.get("/", (req, res) => {
-  res.send("Hellow Express");
+  res.send("Hellow Express da");
+});
+
+app.get("/user/:username", (req, res) => {
+  const username = req.params.username;
+  res.send(`Welcome, ${username}!`);
 });
 
 app.listen(PORT, () => {
